84f0a9708b2a748cc3f1b2b135715b3529e14f6d
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-06-08 09:34:02 -0700

    Removed eval, and restructured object to be able to access character properties with string key.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,198 +7,111 @@ let playerHealthHTML = document.getElementById("playerHP");
 let counterAttackHTML = document.getElementById("computerCounterAttack");
 let computerHealthHTML = document.getElementById("computerHealth");
 
-class Controller {
-
-    constructor() {
-      this.newAttack = 0;
-      this.defeatedEnemies = 0;
-      this.peas = {
-        name: "peas",
-        attack : 5,
-        health : 100,
-        counterAttack : 10,
-      };
-      this.broccoli = {
-        name : "broccoli",
-        attack : 5,
-        health : 100,
-        counterAttack : 10,
-      };
-      this.dragonfruit = {
-        name : "dragonfruit",
-        attack : 5,
-        health : 100,
-        counterAttack : 10,
-      };
-      this.carrots = {
-        name : "carrots",
-        attack : 5,
-        health : 100,
-        counterAttack : 10
-      };
-      this.tomato = {
-        name : "tomato",
-        attack : 5,
-        health : 100,
-        counterAttack : 10,
-      };
-
-    }
-  
-    battleFunction(player, computer) {
-      //increments playerAttack
-      this.newAttack += this[player].attack;
-      this[player].health -= this[computer].counterAttack;
-  
-      //deducts attack from computer HP
-      if (this.newAttack > this[player].attack) {
-        this[computer].health -= this.newAttack;
-        console.log(this[computer].health);
-      } else {
-        this[computer].health -= this[player].attack;
-        console.log(this[computer].health);
-      }
-  
-      if (this[computer].health <= 0) {
-        this.defeatedEnemies++
-      }
-      if (this.defeatedEnemies >= 4) {
-        alert("You won!");
-        //resetGame();
-      }
-    };
-  
-}
-  
-  const ctrl = new Controller();
-  $("#fightBtn").click(() => {
-    ctrl.battleFunction($("#playerDiv").children("img").attr("id"), $("#computerDiv").children("img").attr("id"));
-  });
-
-
-
-
 //fighting objects
 
-// let peas = {
-//     name : "peas",
-//     attack : 5,
-//     health : 100,
-//     counterAttack : 10,
-//     enemy : false
-// };
-// let broccoli = {
-//     name : "broccoli",
-//     attack : 5,
-//     health : 100,
-//     counterAttack : 10,
-//     enemy : false
-// };
-// let dragonfruit = {
-//     name : "dragonfruit",
-//     attack : 5,
-//     health : 100,
-//     counterAttack : 10,
-//     enemy : false
-// };
-// let carrots = {
-//     name : "carrots",
-//     attack : 5,
-//     health : 100,
-//     counterAttack : 10
-// };
-// let tomato = {
-//     name : "tomato",
-//     attack : 5,
-//     health : 100,
-//     counterAttack : 10,
-//     enemy : false
-// };
+let chars = {
+"peas": {
+     name: "peas",
+     attack : 5,
+     health : 100,
+     counterAttack : 10,
+  },
+  "broccoli": {
+    attack : 5,
+    health : 100,
+    counterAttack : 10,
+  },
+  "dragonfruit": {
+    attack : 5,
+    health : 100,
+    counterAttack : 10,
+  },
+  "carrots": {
+    attack : 5,
+    health : 100,
+    counterAttack : 10,
+  },
+  "tomato": {
+    attack : 5,
+    health : 100,
+    counterAttack : 10,
+  }
+};
 
+let defeatedEnemies = 0;
+let newAttack = 0;
 
-// //
-// let playerSelected;
-// let computerSelected;
-
-// //fight values
-// let defeatedEnemies = 0;
-// let newAttack = 0;
-
-// function resetGame() {
+function resetGame() {
     
-//     //resetsObjects
-//     peas.name = "Peas";
-//     peas.attack = 5;
-//     peas.health = 150;
-//     peas.counterAttack = 10;
-//     peas.enemy = false;
+    //resetsObjects
+    chars["peas"].attack = 5;
+    chars["peas"].health = 150;
+    chars["peas"].counterAttack = 10;
 
-//     broccoli.name = "Peas";
-//     broccoli.attack = 5;
-//     broccoli.health = 100;
-//     broccoli.counterAttack = 10;
-//     broccoli.enemy = false;
+    
+    chars["broccoli"].attack = 5;
+    chars["broccoli"].health = 100;
+    chars["broccoli"].counterAttack = 10;
 
-//     dragonfruit.name = "Peas";
-//     dragonfruit.attack = 5;
-//     dragonfruit.health = 100;
-//     dragonfruit.counterAttack = 10;
-//     dragonfruit.enemy = false;
+    
+    chars["dragonfruit"].attack = 5;
+    chars["dragonfruit"].health = 100;
+    chars["dragonfruit"].counterAttack = 10;
 
-//     carrots.name = "Peas";
-//     carrots.attack = 5;
-//     carrots.health = 100;
-//     carrots.counterAttack = 10;
-//     carrots.enemy = false;
+    
+    chars["carrots"].attack = 5;
+    chars["carrots"].health = 100;
+    chars["carrots"].counterAttack = 10;
 
-//     tomato.name = "Peas";
-//     tomato.attack = 5;
-//     tomato.health = 100;
-//     tomato.counterAttack = 10;
-//     tomato.enemy = false;
+    
+    chars["tomato"].attack = 5;
+    chars["tomato"].health = 100;
+    chars["tomato"].counterAttack = 10;
 
-//     //resets Attack Value
-//     newAttack = 0;
-//     defeatedEnemies = 0;
+    //resets Attack Value
+    newAttack = 0;
+    defeatedEnemies = 0;
 
-//      $("#playNow").show();
-// }
+     $("#playNow").show();
+     console.log("I reset everything but you didn't update the HTML.");
+}
 
-// $("#fightBtn").click( () => {
-//     console.log($("#playerDiv").children("img").attr("id"));
-//     console.log($("#computerDiv").children("img").attr("id")); 
-//     battleFunction(eval($("#playerDiv").children("img").attr("id")), eval($("#computerDiv").children("img").attr("id")));
-// });
+$("#fightBtn").click( () => {
+    battleFunction($("#playerDiv").children("img").attr("id"), $("#computerDiv").children("img").attr("id"));
+});
     
     
-//  function battleFunction(player, computer) {
-//     //increments playerAttack
-//     newAttack += player.attack;
-//     player.health -= computer.counterAttack;
+ function battleFunction(player, computer) {
+    //increments playerAttack
+    newAttack += chars[player].attack;
+    chars[player].health -= chars[computer].counterAttack;
 
-//     //deducts attack from computer HP
-//     if (newAttack > player.attack) {
-//         computer.health -= newAttack;
-//         if (computer.health <= 0) {
-//             defeatedEnemies++;
-//         }
 
-//     } else {
-//         computer.health -= player.attack;
-//     }
 
-//     playerAttackHTML.textContent = "Attack Power: " + newAttack;
-//     playerHealthHTML.textContent = "Health Points: " + player.health;
+    //deducts attack from computer HP
+    if (newAttack > chars[player].attack) {
+        chars[computer].health -= newAttack;
+        if (chars[computer].health <= 0) {
+            defeatedEnemies++;         
+        }
 
-//     counterAttackHTML.textContent = "Counter Attack Power: " + computer.counterAttack;
-//     computerHealthHTML.textContent = "Health Points: " + computer.health;
+    } else {
+        chars[computer].health -= chars[player].attack;
+    }
 
-//     if (defeatedEnemies >= 4) {
-//         alert("You won!");
-//         resetGame();
-//         console.log(computer.health);
-//     }
-// };
+    //updates HTML with stats real-time.
+    playerAttackHTML.textContent = "Attack Power: " + newAttack;
+    playerHealthHTML.textContent = "Health Points: " + chars[player].health;
+
+    counterAttackHTML.textContent = "Counter Attack Power: " + chars[computer].counterAttack;
+    computerHealthHTML.textContent = "Health Points: " + chars[computer].health;
+    
+    //win condition
+    if (defeatedEnemies >= 4) {
+        alert("You won!");
+        resetGame();
+    }
+};
 
 //puts images in their respective sides of the screen on click
 $(".imgStyles").click(function (event) {
@@ -206,14 +119,14 @@ $(".imgStyles").click(function (event) {
     if ($("#playerDiv").find('img').length == 1 && $("#computerDiv").find('img').length === 0) {
         computerSelected = event.target.id;
         $(event.currentTarget).appendTo("#computerDiv");
-        counterAttackHTML.textContent = "Counter Attack Power: " + eval(computerSelected).counterAttack;
-        computerHealthHTML.textContent = "Health Points: " + eval(computerSelected).health;
+        counterAttackHTML.textContent = "Counter Attack Power: " + chars[computerSelected].counterAttack;
+        computerHealthHTML.textContent = "Health Points: " + chars[computerSelected].health;
 
     } else if ($("#playerDiv").find('img').length == 0) {
         $(event.currentTarget).appendTo("#playerDiv");
         playerSelected = event.target.id;
-        playerAttackHTML.textContent = "Attack Power: " + eval(playerSelected).attack;
-        playerHealthHTML.textContent = "Health Points: " + eval(playerSelected).health;
+        playerAttackHTML.textContent = "Attack Power: " + chars[playerSelected].attack;
+        playerHealthHTML.textContent = "Health Points: " + chars[playerSelected].health;
     }
 });
 
