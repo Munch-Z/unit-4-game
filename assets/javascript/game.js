d686abf60930faf9bed80e36a75775e8f548f2b3
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-06-12 20:10:22 -0700

    It works. Visuals could use some love, and may recade to use classes so it's more dynamic, but it is w orking now.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,29 +11,29 @@ let computerHealthHTML = document.getElementById("computerHealth");
 
 let chars = {
 "peas": {
-     attack : 5,
-     health : 100,
-     counterAttack : 10,
+     attack : 50,
+     health : 200,
+     counterAttack : 50,
   },
   "broccoli": {
-    attack : 5,
-    health : 100,
-    counterAttack : 10,
+    attack : 40,
+    health : 210,
+    counterAttack : 40,
   },
   "dragonfruit": {
-    attack : 5,
-    health : 100,
-    counterAttack : 10,
+    attack : 30,
+    health : 290,
+    counterAttack : 30,
   },
   "carrots": {
-    attack : 5,
-    health : 100,
+    attack : 10,
+    health : 400,
     counterAttack : 10,
   },
   "tomato": {
-    attack : 5,
-    health : 100,
-    counterAttack : 10,
+    attack : 20,
+    health : 340,
+    counterAttack : 20,
     
   }
 };
@@ -83,13 +83,6 @@ function resetGame() {
 
      //re-adds images to top div
      $('#imgsDiv').html('<img src="assets/images/peas.jpeg" id="peas" class="imgStyles" alt="peas"><img src="assets/images/broccoli.jpeg" id="broccoli" class="imgStyles" alt="broccoli"> <img src="assets/images/dragonfruit.jpeg" id="dragonfruit" class="imgStyles" alt="dragonfruit"> <img src="assets/images/carrots.jpeg" id="carrots" class="imgStyles" alt="carrots"> <img src="assets/images/tomato.jpeg" id="tomato" class="imgStyles" alt="tomato">');
-
-
-    //  $('#imgsDiv').append('<img src="assets/images/peas.jpeg" id="peas" alt="peas">').addClass("imgStyles");
-    //  $('#imgsDiv').append('<img src="assets/images/broccoli.jpeg" id="broccoli" alt="broccoli">').addClass("imgStyles");
-    //  $('#imgsDiv').append('<img src="assets/images/dragonfruit.jpeg" id="dragonfruit" alt="dragonfruit">').addClass("imgStyles");
-    //  $('#imgsDiv').append('<img src="assets/images/carrots.jpeg" id="carrots" alt="carrots">').addClass("imgStyles");
-    //  $('#imgsDiv').append('<img src="assets/images/tomato.jpeg" id="tomato" alt="tomato">').addClass("imgStyles");
      
      //resets text areas
      playerAttackHTML.textContent = "";
@@ -97,6 +90,8 @@ function resetGame() {
      counterAttackHTML.textContent = "";
      computerHealthHTML.textContent = "";
      $("#alertSpace").text('');
+     $("#computerAlertSpace").text('');
+     $("#alertHeader").text('');
 
      gameRunning = false;
 }
@@ -104,7 +99,7 @@ function resetGame() {
 $("#playNow").on("click", () => {
   gameRunning = true;
   $("#fightBtn").show();
-  $("#alertSpace").text('Pick your VEGETABLE (or Dragonfruit, if you\'re into that sort of thing.');
+  $("#alertHeader").text('Pick your VEGETABLE (or Dragonfruit, if you\'re into that sort of thing.)');
   $("#playNow").hide();
   console.log(gameRunning);
 })
@@ -117,18 +112,24 @@ $("#fightBtn").click( () => {
     
     
  function battleFunction(player, computer) {
-    //increments playerAttack
+    
+  //increments playerAttack
     newAttack += chars[player].attack;
     chars[player].health -= chars[computer].counterAttack;
 
+    $("#computerCounterAttack").text("Counter Attack Power: " + chars[computer].counterAttack);
+    $("#computerHealth").text("Health Points: " + chars[computer].health);
 
 
     //deducts attack from computer HP
     if (newAttack > chars[player].attack) {
         chars[computer].health -= newAttack;
+        $("#playerAttack").text("Attack Power: " + (newAttack + chars[player].attack));
         if (chars[computer].health <= 0) {
             defeatedEnemies++;
-            $("#computerDiv").children("img").detach();       
+            $("#computerCounterAttack").text("");
+            $("#computerHealth").text("");
+            $("#computerDiv").children("img").detach();   
         }
 
     } else {
@@ -136,23 +137,30 @@ $("#fightBtn").click( () => {
     }
 
     //updates HTML with stats real-time.
-    $("#playerAttack").text("Attack Power: " + newAttack);
     $("#playerHP").text("Health Points: " + chars[player].health);
 
-    $("#computerCounterAttack").text("Counter Attack Power: " + chars[computer].counterAttack);
-    $("#computerHealth").text("Health Points: " + chars[computer].health);
+    $("#alertSpace").html(player.toUpperCase() + ' did ' + newAttack + ' damage to ' + computer.toUpperCase() + '<br>' + computer.toUpperCase() + ' did ' + chars[computer].counterAttack + ' damage to ' + player.toUpperCase());
+    
     
     //win condition
-    if (defeatedEnemies >= 4) {
+    setTimeout(() => {
+      console.log('I happened')
+      if (defeatedEnemies >= 4) {
         alert("You won!");
         $("#playerDiv").children("img").detach();
+        $("#fightBtn").hide()
         resetGame();
-    }
+      }}, 100);
 
-    if (chars[player].health <= 0) {
-      alert("You lost!");
-      resetGame();
-    }
+    setTimeout(() => {
+      console.log('No it was me.')
+      if (chars[player].health <= 0) {
+        alert("You lost!");
+        resetGame();
+        $("#fightBtn").hide()
+
+      }
+     }, 100);
 };
 
 //puts images in their respective sides of the screen on click
@@ -160,20 +168,19 @@ $("#fightBtn").click( () => {
 
 
   $("#imgsDiv").on('click', ".imgStyles", function (event) {
-    console.log("I detected the class of imgStyles being clicked");
     
     if (gameRunning){
       
-      if ($("#playerDiv").find('img').length == 1 && $("#computerDiv").find('img').length === 0) {
-        console.log("I'm working, you're just an idiot.")
+      if ($("#playerDiv").find('img').length == 1 && $("#computerDiv").find('img[data-value=active]:visible').length === 0) {
         computerSelected = event.target.id;
-        $(event.currentTarget).appendTo("#computerDiv");
+        $("#alertHeader").text('FIGHT!');
+        $(event.currentTarget).attr("data-value", "active").prependTo("#computerDiv");
         counterAttackHTML.textContent = "Counter Attack Power: " + chars[computerSelected].counterAttack;
         computerHealthHTML.textContent = "Health Points: " + chars[computerSelected].health;
 
       } else if ($("#playerDiv").find('img').length == 0) {
-        console.log("I'm working, you're just an idiot.")
-        $(event.currentTarget).appendTo("#playerDiv");
+        $(event.currentTarget).attr("data-value", "active").appendTo("#playerDiv");
+        $("#alertHeader").text('Pick your opponent!');
         playerSelected = event.target.id;
         playerAttackHTML.textContent = "Attack Power: " + chars[playerSelected].attack;
         playerHealthHTML.textContent = "Health Points: " + chars[playerSelected].health;
